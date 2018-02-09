c21d1d3643e3cb2e00e9129d588da72467dbb919
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-02-09 14:18:35 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,f=1;
+    printf("enter the value");
+    scanf("%d",&n);
+    for(i=1;i<=2;i++)
+    {
+        f=f*i;
+        {
+            printf("%d",&i);
+        }
+    }
